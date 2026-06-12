082b46f0127a7dc7c615957de0532b00cfed4c3b
Anushka Eranga <hello@aefotograph.art>
2026-06-12 22:27:29 +0400

    Initial Commit

---
+++ AEQPro/AEQProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AEQProApp.swift
+//  AEQPro
+//
+//  Created by Anushka Eranga on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct AEQProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
